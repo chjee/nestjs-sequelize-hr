7aab68b2b52a1d5209a8674678b86817d9c108b3
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-21 16:14:40 +0900

    Appended NestFactory properties.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+    cors: { origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' },
+  });
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
