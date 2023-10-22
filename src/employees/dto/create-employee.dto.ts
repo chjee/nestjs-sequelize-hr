d2f90ae11ae20c59f66cd6e98543f337d57947ea
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-22 19:59:51 +0900

    Registered some employee DTOs.

---
+++ src/employees/dto/create-employee.dto.ts
@@ -0,0 +1,57 @@
+import {
+  IsDate,
+  IsDecimal,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Length,
+  Min,
+} from 'class-validator';
+
+export class CreateEmployeeDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  readonly employee_id: number;
+
+  @IsString()
+  @Length(2, 20)
+  readonly first_name?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 25)
+  readonly last_name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @Length(2, 25)
+  readonly email: string;
+
+  @IsString()
+  @Length(2, 20)
+  readonly phone_number?: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  readonly hire_date: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 10)
+  readonly job_id: string;
+
+  @IsNotEmpty()
+  @IsDecimal()
+  readonly salary: number;
+
+  @IsDecimal()
+  readonly commission_pct?: number;
+
+  @IsNumber()
+  readonly manager_id?: number;
+
+  @IsNumber()
+  readonly department_id?: number;
+}
