b5a732517c4d5f698f1c3283990825b2ae7970c9
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-22 17:12:03 +0900

    Applied a ValidationPipe function.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
     cors: { origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' },
   });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
