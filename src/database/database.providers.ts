1734db86588d105a5744c1b74d9ae12e3289d708
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-23 20:36:21 +0900

    Added some Models to database.provider.

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,6 +1,9 @@
 import { Sequelize } from 'sequelize-typescript';
 import { Department } from '../departments/entities/department.entity';
 import { Employee } from '../employees/entities/employee.entity';
+import { Location } from '../departments/entities/location.entity';
+import { Country } from 'src/departments/entities/country.entity';
+import { Region } from 'src/departments/entities/region.entity';
 
 export const databaseProviders = [
   {
@@ -17,7 +20,7 @@ export const databaseProviders = [
           timestamps: false,
         },
       });
-      sequelize.addModels([Department, Employee]);
+      sequelize.addModels([Department, Employee, Location, Country, Region]);
       await sequelize.sync();
       return sequelize;
     },
