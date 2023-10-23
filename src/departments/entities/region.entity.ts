90f01797a665d9d10c3570106ec2e15cd96dcad2
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-23 19:31:51 +0900

    Modified a @Column of region entity model.

--- src/departments/entities/region.entity.ts
+++ src/departments/entities/region.entity.ts
@@ -6,19 +6,20 @@ import {
   Default,
   PrimaryKey,
   Comment,
+  DataType,
 } from 'sequelize-typescript';
 
 @Table({ tableName: 'regions' })
 export class Region extends Model<Region> {
-  @Column
   @Comment('')
-  @AllowNull(false)
   @PrimaryKey
+  @AllowNull(false)
+  @Column(DataType.BIGINT.UNSIGNED)
   region_id: number;
 
-  @Column
   @Comment('')
   @AllowNull(true)
   @Default(null)
+  @Column(DataType.STRING(25))
   region_name?: string;
 }
