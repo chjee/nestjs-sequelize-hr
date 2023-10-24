60fcb5cbce5586ab2a8fad6fe3caf26ff8b2341b
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-24 16:36:38 +0900

    Added async/await & exception statements.

--- src/departments/departments.service.ts
+++ src/departments/departments.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { Department } from './entities/department.entity';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { UpdateDepartmentDto } from './dto/update-department.dto';
@@ -11,31 +11,41 @@ export class DepartmentsService {
     @Inject('DEPARTMENT_REPOSITORY')
     private readonly departmentRepository: typeof Department,
   ) {}
-  async create(createDepartmentDto: CreateDepartmentDto) {
-    return this.departmentRepository.create<Department>(createDepartmentDto);
+  async create(createDepartmentDto: CreateDepartmentDto): Promise<Department> {
+    const department =
+      await this.departmentRepository.create<Department>(createDepartmentDto);
+    return department;
   }
 
   async findAll(): Promise<Department[]> {
-    return this.departmentRepository.findAll<Department>({
+    const departments = await this.departmentRepository.findAll<Department>({
       include: [Employee, Location],
     });
+    if (!departments) {
+      throw new HttpException('No departments found', HttpStatus.NOT_FOUND);
+    }
+    return departments;
   }
 
   async findOne(id: number): Promise<Department> {
-    return this.departmentRepository.findOne({
+    const department = await this.departmentRepository.findOne<Department>({
       include: [Employee, Location],
       where: { department_id: id },
     });
+    if (!department) {
+      throw new HttpException('No department found', HttpStatus.NOT_FOUND);
+    }
+    return department;
   }
 
   async update(id: number, updateDepartmentDto: UpdateDepartmentDto) {
-    return this.departmentRepository.update(updateDepartmentDto, {
+    return await this.departmentRepository.update(updateDepartmentDto, {
       where: { department_id: id },
     });
   }
 
   async remove(id: number) {
-    return this.departmentRepository.destroy({
+    return await this.departmentRepository.destroy({
       where: { department_id: id },
     });
   }
