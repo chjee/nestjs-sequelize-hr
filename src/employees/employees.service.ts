616b88b6ce8f413683a396b7bf4d7a2826db9ba7
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-24 16:42:33 +0900

    Added async/await & exception statements.

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { Employee } from './entities/employee.entity';
@@ -12,31 +12,41 @@ export class EmployeesService {
     private readonly employeeRepository: typeof Employee,
   ) {}
 
-  async create(createEmployeeDto: CreateEmployeeDto) {
-    return this.employeeRepository.create<Employee>(createEmployeeDto);
+  async create(createEmployeeDto: CreateEmployeeDto): Promise<Employee> {
+    const employee =
+      await this.employeeRepository.create<Employee>(createEmployeeDto);
+    return employee;
   }
 
-  async findAll() {
-    return this.employeeRepository.findAll<Employee>({
+  async findAll(): Promise<Employee[]> {
+    const employees = await this.employeeRepository.findAll<Employee>({
       include: [Employee, Job, Department],
     });
+    if (!employees) {
+      throw new HttpException('No employees found', HttpStatus.NOT_FOUND);
+    }
+    return employees;
   }
 
-  async findOne(id: number) {
-    return this.employeeRepository.findOne({
+  async findOne(id: number): Promise<Employee> {
+    const employee = await this.employeeRepository.findOne<Employee>({
       include: [Employee, Job, Department],
       where: { employee_id: id },
     });
+    if (!employee) {
+      throw new HttpException('No employee found', HttpStatus.NOT_FOUND);
+    }
+    return employee;
   }
 
   async update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
-    return this.employeeRepository.update(updateEmployeeDto, {
+    return await this.employeeRepository.update(updateEmployeeDto, {
       where: { employee_id: id },
     });
   }
 
   async remove(id: number) {
-    return this.employeeRepository.destroy({
+    return await this.employeeRepository.destroy({
       where: { employee_id: id },
     });
   }
