2237293900a7c675fbba28b8870e98c0c36832f8
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-26 18:23:04 +0900

    Added a ConfigModule & properties.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DepartmentsModule } from './departments/departments.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DepartmentsModule, EmployeesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: ['.env.dev', '.env'],
+    }),
+    DepartmentsModule,
+    EmployeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
