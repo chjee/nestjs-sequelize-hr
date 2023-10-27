0b815e146ef827ebafe85b31ccfd46d6907c9e05
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-27 15:08:47 +0900

    Applied dynamic database properties.

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,23 +6,13 @@ import { Country } from '../departments/entities/country.entity';
 import { Region } from '../departments/entities/region.entity';
 import { Job } from '../employees/entities/job.entity';
 import { JobHistory } from '../employees/entities/jobhistory.entity';
+import config from '../config/configuration';
 
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const sequelize = new Sequelize({
-        dialect: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'echonrich',
-        password: 'echonrich0928',
-        database: 'hr',
-        logging: false,
-        define: {
-          timestamps: false,
-        },
-      });
+      const sequelize = new Sequelize(config().database);
       sequelize.addModels([
         Department,
         Employee,
