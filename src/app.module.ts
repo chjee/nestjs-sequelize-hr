7a2f171ec4caa16007d1729c2432bbf214a59654
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-27 19:17:11 +0900

    Added a AuthModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { DepartmentsModule } from './departments/departments.module';
 import { EmployeesModule } from './employees/employees.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { UsersModule } from './users/users.module';
     DepartmentsModule,
     EmployeesModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
