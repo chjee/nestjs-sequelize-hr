d7beece6d83aeb0fca031e2a3add7c155f7551f7
Changhoon-Jee <Changhoon-Jee@users.noreply.github.com>
2023-10-28 21:30:33 +0900

    Removed  UseGuard decorator.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,23 +6,22 @@ import {
   HttpStatus,
   Post,
   Request,
-  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInUserDto } from '../users/dto/signin-user.dto';
-import { AuthGuard } from './auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInUserDto: SignInUserDto) {
     return this.authService.signIn(signInUserDto);
   }
 
-  @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
