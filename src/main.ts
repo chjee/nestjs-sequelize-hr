64dd7417bebc53121fa7a090e71cef50c7147f27
Changhoon-Jee <chjee71@gmail.com>
2023-10-29 17:34:35 +0900

    Applied swagger config.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -15,6 +16,30 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Oracle HR Example')
+    .setDescription('The HR API description')
+    .setVersion('1.0')
+    .addTag('HR')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        bearerFormat: 'Token',
+        in: 'header',
+      },
+      'access_token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      defaultModelsExpandDepth: -1,
+    },
+  });
 
   const configSerivce = app.get(ConfigService);
   const listenPort = configSerivce.get<number>('PORT', 3000);
