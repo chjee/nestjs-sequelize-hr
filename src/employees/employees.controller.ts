94623673412e7c698348ffe98fb070017fc049c0
Changhoon-Jee <chjee71@gmail.com>
2023-10-29 14:54:33 +0900

    Applied async statements.

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -11,36 +11,39 @@ import {
 import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { Employee } from './entities/employee.entity';
 
 @Controller('employee')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
   @Post()
-  create(@Body() createEmployeeDto: CreateEmployeeDto) {
+  async create(
+    @Body() createEmployeeDto: CreateEmployeeDto,
+  ): Promise<Employee> {
     return this.employeesService.create(createEmployeeDto);
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<Employee[]> {
     return this.employeesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id', new ParseIntPipe()) id: number) {
+  findOne(@Param('id', new ParseIntPipe()) id: number): Promise<Employee> {
     return this.employeesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(
+  async update(
     @Param('id', new ParseIntPipe()) id: number,
     @Body() updateEmployeeDto: UpdateEmployeeDto,
-  ) {
+  ): Promise<any> {
     return this.employeesService.update(+id, updateEmployeeDto);
   }
 
   @Delete(':id')
-  remove(@Param('id', new ParseIntPipe()) id: number) {
+  async emove(@Param('id', new ParseIntPipe()) id: number): Promise<any> {
     return this.employeesService.remove(+id);
   }
 }
