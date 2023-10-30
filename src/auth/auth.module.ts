d9771acc8993ff7e768816f6473cfa3f2fcd5b6e
Changhoon-Jee <chjee71@gmail.com>
2023-10-30 18:31:30 +0900

    Adjusted jwt signOptions.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -12,17 +11,8 @@ import { jwtConstants } from './constants';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '10m' },
     }),
-    // JwtModule.registerAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: async (configSerivce: ConfigService) => ({
-    //     global: true,
-    //     secret: configSerivce.get<string>('JWT_SECRET'),
-    //     signOptions: { expiresIn: '60s' },
-    //   }),
-    //   inject: [ConfigService],
-    // }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
