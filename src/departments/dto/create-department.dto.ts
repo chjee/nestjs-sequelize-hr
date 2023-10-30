c424a6908b6a57d76cc6eda605d66a23c188c3d9
Changhoon-Jee <chjee71@gmail.com>
2023-10-30 16:36:08 +0900

    Added swagger decorators.

--- src/departments/dto/create-department.dto.ts
+++ src/departments/dto/create-department.dto.ts
@@ -1,20 +1,42 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString, Length, Min } from 'class-validator';
 
 export class CreateDepartmentDto {
+  @ApiProperty({
+    description: 'Department ID',
+    example: 11,
+    minimum: 0,
+  })
   @IsNotEmpty()
   @IsNumber()
   @Min(0)
   readonly department_id: number;
 
+  @ApiProperty({
+    description: 'Department Name',
+    example: 'Holding',
+    minLength: 2,
+    maxLength: 30,
+  })
   @IsNotEmpty()
   @IsString()
   @Length(2, 30)
   readonly department_name: string;
 
+  @ApiProperty({
+    description: 'Manager ID',
+    example: 200,
+    minimum: 0,
+  })
   @IsNumber()
   @Min(0)
   readonly manager_id: number;
 
+  @ApiProperty({
+    description: 'Location ID',
+    example: 1700,
+    minimum: 0,
+  })
   @IsNumber()
   @Min(0)
   readonly location_id: number;
