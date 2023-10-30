9cc450c518a9e47a6b20a36f7542c6deb982979c
Changhoon-Jee <chjee71@gmail.com>
2023-10-30 16:33:35 +0900

    Changed decorator order.

--- src/users/dto/signin-user.dto.ts
+++ src/users/dto/signin-user.dto.ts
@@ -2,25 +2,25 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class SignInUserDto {
-  @IsNotEmpty()
-  @IsString()
-  @Length(5, 20)
   @ApiProperty({
     description: 'User ID',
     example: 'admin',
     minLength: 5,
     maxLength: 20,
   })
-  readonly userid!: string;
-
   @IsNotEmpty()
   @IsString()
-  @Length(5, 12)
+  @Length(5, 20)
+  readonly userid!: string;
+
   @ApiProperty({
     description: 'User Password',
     example: 'whoami',
     minLength: 5,
     maxLength: 12,
   })
+  @IsNotEmpty()
+  @IsString()
+  @Length(5, 12)
   readonly password!: string;
 }
